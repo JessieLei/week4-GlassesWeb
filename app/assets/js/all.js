bac4ed04e8573816f3afa870eef53b494705afd7
JessieLei <alen31002000@gmail.com>
2022-08-01 19:01:23 +0800

    第一版

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,10 @@
-console.log('Hello!');
+/* console.log('Hello!');
 
 $(document).ready(() => {
   console.log('HesSchool Hello!');
+}); */
+
+$(".ham-icon").click(function (e) {
+  e.preventDefault();
+  $(".dropdown").toggleClass("active");
 });
