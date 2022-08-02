61c910607c6f5b6dac1c1cff8da1c3f2ffd9159c
JessieLei <alen31002000@gmail.com>
2022-08-03 00:43:08 +0800

    Update 2022-08-02T16:43:05.787Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -9,6 +9,11 @@ $(".ham-icon").click(function (e) {
   e.preventDefault();
   $(".dropdown").toggleClass("active");
 });
+$(".top-navbar").click(function (e) {
+  e.preventDefault();
+  $(this).addClass("this-page-active");
+  $(this).parent().siblings().children().removeClass("this-page-active");
+});
 "use strict";
 
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
