9851c18f88ae2134449fa20d11d8b94ce570c2e8
JessieLei <alen31002000@gmail.com>
2022-08-03 00:46:00 +0800

    week4第一版

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -8,3 +8,9 @@ $(".ham-icon").click(function (e) {
   e.preventDefault();
   $(".dropdown").toggleClass("active");
 });
+
+$(".top-navbar").click(function (e) {
+  e.preventDefault();
+  $(this).addClass("this-page-active");
+  $(this).parent().siblings().children().removeClass("this-page-active");
+});
